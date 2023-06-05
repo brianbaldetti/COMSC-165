22459f6e74b119c02dc7e26174d53f11fe03e9cf
Brian <135410817+brianbaldetti@users.noreply.github.com>
2023-06-04 19:06:26 -0700

    Logic Program
    Program implements some very basic logical operators.

---
+++ ReviewControlStructures/Logic.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Exercise2E
+//
+//  Created by brian on 6/19/22.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 2000000, num2 = 30000;
+    int sum = num1 + num2;
+    
+    if (sum > 40000)
+        cout << "greater than 40,000\n";
+    
+    return 0;
+}
