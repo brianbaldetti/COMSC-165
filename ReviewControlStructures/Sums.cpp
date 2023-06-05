d65a6034708db793a0415b0c694a9f97f636e5bb
Brian <135410817+brianbaldetti@users.noreply.github.com>
2023-06-04 19:05:29 -0700

    Sums Program
    Sums two user inputted numbers.

---
+++ ReviewControlStructures/Sums.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Exercise1B
+//
+//  Created by brian on 6/19/22.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Enter a number: ";
+    cin >> num1;
+    cout << "Enter another: ";
+    cin >> num2;
+    cout << "The sum is: " << num1 + num2 << endl;
+    
+    return 0;
+}
