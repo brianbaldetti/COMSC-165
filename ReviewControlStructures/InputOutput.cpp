3e6682154c3b9d8687110d60d7adb366835bcb4a
Brian <135410817+brianbaldetti@users.noreply.github.com>
2023-06-04 19:04:49 -0700

    Input Output Program
    Receivers a users input and then outputs directly back.

---
+++ ReviewControlStructures/InputOutput.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Exercise1A
+//
+//  Created by brian on 6/19/22.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Input two numbers seperated by spaces: \n";
+    int x, y;
+    cin >> x >> y;
+    cout << "The numbers inputed were: " << y << " and " << x << endl;
+    
+    return 0;
+}
